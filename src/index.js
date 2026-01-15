ae8ee9d3f18c719149e00000aaaf7a5ef8048523
MikaPoikonen <mikapoi@metropolia.fi>
2026-01-15 10:14:47 +0200

    Implement basic Express server with a welcome route

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-console.log("Tere päivää")
-console.log("Tere päivää")
+import express from 'express';
+const hostname = '127.0.0.1';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 //npm run dev käynnistää scriptin ja 
 // päivittää automaattisesti
