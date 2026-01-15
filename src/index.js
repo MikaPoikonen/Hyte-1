9fef7aad5a67c96c7db3489f0aac1eefb8401e15
MikaPoikonen <mikapoi@metropolia.fi>
2026-01-15 10:25:35 +0200

    Refactor server implementation to use http module instead of express

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import express from 'express';
+import http from 'http';
 const hostname = '127.0.0.1';
-const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my REST API!');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Welcome to my REST API!');
 });
 
-app.listen(port, hostname, () => {
+server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 //npm run dev käynnistää scriptin ja 
