67cf561f7d29cb089ccd66104fd082f13ffa7f31
MikaPoikonen <mikapoi@metropolia.fi>
2026-01-15 10:52:15 +0200

    Enhance server response to include requested URL and improve logging

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+    console.log('Request:', req.url, req.method, req.headers);
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to my REST API!');
+  res.end(`<h1>Welcome to my REST API!</h1><p> Haettu url ${req.url}</p>`);
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
 //npm run dev käynnistää scriptin ja 
 // päivittää automaattisesti
+// res end haetaan nyt url pyynnöt ja tulostetaan consoleen
